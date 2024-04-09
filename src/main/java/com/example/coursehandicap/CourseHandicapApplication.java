b2256b9300619abb21c099acbb1d172b5b9abc42
sixayed <d.p.martyshkin@gmail.com>
2024-04-09 12:45:02 +0700

    Initial commit

---
+++ src/main/java/com/example/coursehandicap/CourseHandicapApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursehandicap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHandicapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseHandicapApplication.class, args);
+    }
+
+}
